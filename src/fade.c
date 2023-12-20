871e3938c48fda5e8bbd66c41e11bef08850ed24
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-19 22:40:18 -0500

    feat: add lots of `asm` segments suggested by Splat

--- src/fade.c
+++ src/fade.c
@@ -1,7 +1,13 @@
 #include "c64.h"
 #include <ultra64.h>
 
+// extern unknown_struct D_80383AB8;
+
 #pragma GLOBAL_ASM("../asm/nonmatchings/fade/fade_setAllFlags.s")
+
+// void fade_setAllFlags(s16 flags) {
+//     D_80383AB8.fade_settings.flags = flags;
+// }
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/fade/fade_setFlag.s")
 
