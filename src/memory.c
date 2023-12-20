d253513a7e8c5a6f34ffb64829883fe2ee22d9eb
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-19 22:51:26 -0500

    feat: begin splitting `src/memory.c`

---
+++ src/memory.c
@@ -0,0 +1,81 @@
+#include "c64.h"
+#include <ultra64.h>
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/memory_clear.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/memory_copy.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_init.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_free.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_writebackDCache.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/initHeaps.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80000D68.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80000D74.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_alloc.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_allocWithAlignment.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heapBlock_updateBlockMaxSize.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001008.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heapBlock_free.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001080.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800010A0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800010C8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001110.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001148.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800011AC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800011DC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001228.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001260.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800012C0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800012D8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800012F4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001338.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_curLevel_goToNextFunc.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_prevLevel_goToNextFunc.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_nextLevel_goToNextFunc.s")
+
+#pragma GLOBAL_ASM(                                                            \
+    "../asm/nonmatchings/memory/module_curLevel_goToNextFuncAndClearTimer.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_curLevel_goToPrevFunc.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_prevLevel_goToPrevFunc.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_nextLevel_goToPrevFunc.s")
+
+#pragma GLOBAL_ASM(                                                            \
+    "../asm/nonmatchings/memory/module_curLevel_goToPrevFuncAndClearTimer.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_curLevel_goToFunc.s")
+
+#pragma GLOBAL_ASM(                                                            \
+    "../asm/nonmatchings/memory/module_curLevel_goToFuncInLevel.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_prevLevel_goToFunc.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/memory/module_nextLevel_goToFunc.s")
