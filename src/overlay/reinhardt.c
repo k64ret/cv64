2ed4fa1f228f16ae60634b23fdac17bd730fbb4c
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-24 13:07:22 -0500

    feat(overlay): get `reinhardt.c` and `reinhardt_attackmgr.c` compiling

---
+++ src/overlay/reinhardt.c
@@ -0,0 +1,210 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D13E0_697040.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D1758_6973B8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D17C8_697428.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D1A38_697698.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2008_697C68.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2048_697CA8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2608_698268.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2724_698384.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2918_698578.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2ACC_69872C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2BFC_69885C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2DF0_698A50.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D2FA0_698C00.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D30D0_698D30.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D3334_698F94.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D3370_698FD0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D33F8_699058.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D3520_699180.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D359C_6991FC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D379C_6993FC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D38A0_699500.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D3ABC_69971C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D3F28_699B88.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D3F48_699BA8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4360_699FC0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4530_69A190.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4670_69A2D0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D47A8_69A408.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4924_69A584.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4AF0_69A750.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4D90_69A9F0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D4E3C_69AA9C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D5094_69ACF4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D53B0_69B010.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D5608_69B268.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D576C_69B3CC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D592C_69B58C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D5944_69B5A4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D5EA4_69BB04.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D6038_69BC98.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D6420_69C080.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D64A0_69C100.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D6740_69C3A0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D6950_69C5B0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D69D8_69C638.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D6CD0_69C930.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D6E8C_69CAEC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D70EC_69CD4C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D718C_69CDEC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D7580_69D1E0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D7800_69D460.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D7A10_69D670.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D7AF8_69D758.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D7CE8_69D948.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D7EE4_69DB44.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D80B4_69DD14.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D81AC_69DE0C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D8220_69DE80.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D8470_69E0D0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D85EC_69E24C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D868C_69E2EC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D87E4_69E444.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D88CC_69E52C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D8938_69E598.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D8AB8_69E718.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9080_69ECE0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9188_69EDE8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D93C4_69F024.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9548_69F1A8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D96A8_69F308.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9778_69F3D8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9938_69F598.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9BD4_69F834.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803D9CBC_69F91C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA120_69FD80.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA40C_6A006C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA438_6A0098.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA5D0_6A0230.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA67C_6A02DC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA6F8_6A0358.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA824_6A0484.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA934_6A0594.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DA9E4_6A0644.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DAAE4_6A0744.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DAC08_6A0868.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DAE6C_6A0ACC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DAF68_6A0BC8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB07C_6A0CDC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB0D8_6A0D38.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB170_6A0DD0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB264_6A0EC4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB2E8_6A0F48.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB37C_6A0FDC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB5CC_6A122C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DB7C0_6A1420.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DBAE4_6A1744.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DBE40_6A1AA0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DC030_6A1C90.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DC35C_6A1FBC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DC54C_6A21AC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DC870_6A24D0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt/func_803DC908_6A2568.s")
+
+// clang-format on
---
+++ src/overlay/reinhardt_attackmgr.c
@@ -0,0 +1,20 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DC980_6A25E0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DC9F0_6A2650.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DCABC_6A271C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DCB24_6A2784.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DCBE8_6A2848.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DCCF4_6A2954.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/reinhardt_attackmgr/func_803DCD54_6A29B4.s")
+
+// clang-format on
