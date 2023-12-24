6aae613aacf9962fd8aef7ff8cf8d6b05c9970a5
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-24 13:38:56 -0500

    feat(overlay): get carrie overlay C files compiling

---
+++ src/overlay/carrie_leftstrip.c
@@ -0,0 +1,16 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/carrie_leftstrip/func_803DD320_6B0210.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/carrie_leftstrip/func_803DD390_6B0280.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/carrie_leftstrip/func_803DD45C_6B034C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/carrie_leftstrip/func_803DD4C8_6B03B8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/carrie_leftstrip/func_803DD590_6B0480.s")
+
+// clang-format on
