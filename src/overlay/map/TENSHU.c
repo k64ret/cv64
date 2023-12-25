ec9e50c8b110add0a23b19d1afc6064e6d372366
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-25 15:11:52 -0500

    feat(overlay): split and compile TENSHU

---
+++ src/overlay/map/TENSHU.c
@@ -0,0 +1,48 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018EB10_6EA480.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018EB80_6EA4F0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018EC88_6EA5F8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018ED14_6EA684.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018ED90_6EA700.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018EFC4_6EA934.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F04C_6EA9BC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F0BC_6EAA2C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F12C_6EAA9C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F234_6EABA4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F318_6EAC88.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F374_6EACE4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F554_6EAEC4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018F9A4_6EB314.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018FDEC_6EB75C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018FEAC_6EB81C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018FEF4_6EB864.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_8018FF64_6EB8D4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_80190058_6EB9C8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_801900E4_6EBA54.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/TENSHU/func_80190120_6EBA90.s")
+
+// clang-format on
