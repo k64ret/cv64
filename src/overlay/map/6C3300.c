fb48a78e08391f667a9adea5534afc857e0a9eea
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-24 21:34:49 -0500

    fix(overlay): forgot to commit C files

---
+++ src/overlay/map/6C3300.c
@@ -0,0 +1,22 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018EB10_6C3300.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018EB80_6C3370.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018EBE8_6C33D8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018ED24_6C3514.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018EEE8_6C36D8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018F008_6C37F8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018F0E4_6C38D4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6C3300/func_8018F310_6C3B00.s")
+
+// clang-format on
---
+++ src/overlay/map/MEIRO_TEIEN.c
@@ -0,0 +1,8 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/MEIRO_TEIEN/func_8018EB10_6C50E0.s")
+
+// clang-format on
