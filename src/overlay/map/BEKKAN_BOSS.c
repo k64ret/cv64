ce304a375318964180536e4e6d5b44ec5285203a
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-25 15:33:15 -0500

    feat(overlay): split and compile BEKKAN_BOSS

---
+++ src/overlay/map/BEKKAN_BOSS.c
@@ -0,0 +1,38 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018EB10_6F11B0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018EB80_6F1220.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018ECE4_6F1384.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018EDB0_6F1450.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018EDFC_6F149C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018EEE0_6F1580.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018EF30_6F15D0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F000_6F16A0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F008_6F16A8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F078_6F1718.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F1A8_6F1848.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F234_6F18D4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F274_6F1914.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F33C_6F19DC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F37C_6F1A1C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/BEKKAN_BOSS/func_8018F44C_6F1AEC.s")
+
+// clang-format on
