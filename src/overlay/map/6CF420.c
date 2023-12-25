09452c9d2f0ebde556b3ef43b42ad8b0cf972256
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-12-24 22:28:51 -0500

    feat(overlay): split and compile files for SHOKEI_TOU

---
+++ src/overlay/map/6CF420.c
@@ -0,0 +1,80 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018EB10_6CF420.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018ED38_6CF648.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018EEB4_6CF7C4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018EF10_6CF820.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F174_6CFA84.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F230_6CFB40.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F2A0_6CFBB0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F4DC_6CFDEC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F610_6CFF20.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F8A0_6D01B0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F91C_6D022C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F994_6D02A4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018F9F0_6D0300.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8018FA60_6D0370.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8019013C_6D0A4C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80190784_6D1094.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_801907C4_6D10D4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80190834_6D1144.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80190BA8_6D14B8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80190C18_6D1528.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80190CC4_6D15D4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80190D64_6D1674.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_801910CC_6D19DC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191144_6D1A54.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8019126C_6D1B7C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_801912AC_6D1BBC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_8019131C_6D1C2C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191540_6D1E50.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191644_6D1F54.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_801916D0_6D1FE0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191A5C_6D236C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191AF4_6D2404.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191E38_6D2748.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191EA4_6D27B4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191ECC_6D27DC.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191F14_6D2824.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/6CF420/func_80191F1C_6D282C.s")
+
+// clang-format on
