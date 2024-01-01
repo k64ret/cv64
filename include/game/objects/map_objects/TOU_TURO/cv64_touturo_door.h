ade1139bc51b8ed24f1fe37a3e9499ad75609883
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-01-01 15:39:34 -0500

    fix(overlay): try restoring TOU_TURO `data` section

--- include/game/objects/map_objects/TOU_TURO/cv64_touturo_door.h
+++ include/game/objects/map_objects/TOU_TURO/cv64_touturo_door.h
@@ -7,8 +7,10 @@
 #include "object.h"
 
 // To-do: Un-hardcode these
-#define CV64_TOUTURO_DOOR_EXIT_DLIST     0x0600A300
-#define CV64_TOUTURO_DOOR_ENTRANCE_DLIST 0x0600AB00
+// #define CV64_TOUTURO_DOOR_EXIT_DLIST     0x0600A300
+// #define CV64_TOUTURO_DOOR_ENTRANCE_DLIST 0x0600AB00
+extern const u32 CV64_TOUTURO_DOOR_EXIT_DLIST;
+extern const u32 CV64_TOUTURO_DOOR_ENTRANCE_DLIST;
 
 // ID: 0x1224
 typedef struct cv64_touturo_door {
@@ -40,17 +42,21 @@ cv64_touturo_door_entrance_check_event_flags(cv64_touturo_door_t* self);
 extern void cv64_touturo_door_entrance_close_door(cv64_touturo_door_t* self);
 
 // .data
-cv64_touturo_door_func_t cv64_touturo_door_funcs[] = {cv64_touturo_door_init,
-                                                      cv64_touturo_door_loop};
+// cv64_touturo_door_func_t cv64_touturo_door_funcs[] = {cv64_touturo_door_init,
+//                                                       cv64_touturo_door_loop};
+extern cv64_touturo_door_func_t cv64_touturo_door_funcs[];
 
-u32 cv64_touturo_door_dlists[] = {CV64_TOUTURO_DOOR_EXIT_DLIST,
-                                  CV64_TOUTURO_DOOR_ENTRANCE_DLIST};
+// u32 cv64_touturo_door_dlists[] = {CV64_TOUTURO_DOOR_EXIT_DLIST,
+//                                   CV64_TOUTURO_DOOR_ENTRANCE_DLIST};
+extern u32 cv64_touturo_door_dlists[];
 
-cv64_touturo_door_exit_func_t cv64_touturo_door_exit_funcs[] = {
-    cv64_touturo_door_exit_check_event_flags, func_801578FC};
+// cv64_touturo_door_exit_func_t cv64_touturo_door_exit_funcs[] = {
+//     cv64_touturo_door_exit_check_event_flags, func_801578FC};
+extern cv64_touturo_door_exit_func_t cv64_touturo_door_exit_funcs[];
 
-cv64_touturo_door_entrance_func_t cv64_touturo_door_entrance_funcs[] = {
-    cv64_touturo_door_entrance_check_event_flags,
-    cv64_touturo_door_entrance_close_door, func_801578FC};
+// cv64_touturo_door_entrance_func_t cv64_touturo_door_entrance_funcs[] = {
+//     cv64_touturo_door_entrance_check_event_flags,
+//     cv64_touturo_door_entrance_close_door, func_801578FC};
+extern cv64_touturo_door_entrance_func_t cv64_touturo_door_entrance_funcs[];
 
 #endif // CV64_TOU_TURO_DOOR_H
