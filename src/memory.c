ec7f998ee08e534d1774748a7a1e5a97e61bed97
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-01-01 16:25:22 -0500

    refactor: change while-loop to for-loop in `object_prevLevel_goToPrevFunc`

--- src/memory.c
+++ src/memory.c
@@ -144,10 +144,9 @@ void object_prevLevel_goToPrevFunc(u16 current_functionInfo[],
         functionInfo_entry = &current_functionInfo[*functionInfo_ID];
         functionInfo_entry--;
         (*functionInfo_entry)--;
-        entry_to_clean = functionInfo_entry + 1;
-        while (entry_to_clean < (u16*) functionInfo_ID) {
+        for (entry_to_clean = functionInfo_entry + 1;
+             entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
             *entry_to_clean = 0;
-            entry_to_clean++;
         }
     }
 }
