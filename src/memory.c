aa2f810d86abd275b9e96398069bc01d7e17e9e4
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-01-07 15:41:51 -0500

    feat: begin matching `src/memory.c` (#12)
    * feat: match `heap_init`
    
    * feat: match `heap_free`
    
    * fix: use `HEAP_INACTIVE` flag in `heap_free`
    
    * feat: match `object_curLevel_goToNextFunc`
    
    * feat: match `object_prevLevel_goToNextFunc`
    
    * feat: match `object_nextLevel_goToNextFunc`
    
    * feat: match `object_curLevel_goToNextFuncAndClearTimer`
    
    * feat: match `object_curLevel_goToPrevFunc`
    
    * feat: match `object_prevLevel_goToPrevFunc`
    
    * feat: match `object_nextLevel_goToPrevFunc`
    
    * feat: match `object_curLevel_goToPrevFuncAndClearTimer`
    
    * feat: match `object_curLevel_goToFunc`
    
    * feat: match `object_curLevel_goToFuncInLevel`
    
    * feat: match `object_prevLevel_goToFunc`
    
    * feat: match `object_nextLevel_goToFunc`
    
    * refactor: change while-loop to for-loop in `object_prevLevel_goToNextFunc`
    
    * refactor: change while-loop to for-loop in `object_curLevel_goToNextFuncAndClearTimer`
    
    * refactor: change while-loop to for-loop in `object_curLevel_goToPrevFunc`
    
    * refactor: change while-loop to for-loop in `object_prevLevel_goToPrevFunc`
    
    * refactor: change while-loop to for-loop in `object_curLevel_goToPrevFuncAndClearTimer`
    
    * refactor: change while-loop to for-loop in `object_curLevel_goToFunc`
    
    * refactor: change while-loop to for-loop in `object_curLevel_goToFuncInLevel`
    
    * refactor: change while-loop to for-loop in `object_prevLevel_goToFunc`
    
    * A few more matches in memory.c
    
    * Added entry in .gitignore to ignore VSC directory
    
    ---------
    
    Co-authored-by: moisesPC <moisesapc64@gmail.com>

--- src/memory.c
+++ src/memory.c
@@ -1,32 +1,52 @@
-#include "cv64.h"
-#include <ultra64.h>
+#include "memory.h"
+#include "object.h"
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/memory_clear.s")
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/memory_copy.s")
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_init.s")
+void heap_init(cv64_heap_kind_t kind, cv64_heapblock_hdr_t* first_block_ptr,
+               s32 heap_size, u32 additional_flags) {
+    heaps[kind].flags = additional_flags | HEAP_ACTIVE;
+    heaps[kind].size = ALIGN8(heap_size);
+    heaps[kind].heap_start = ALIGN8(first_block_ptr);
+    first_block_ptr->flags = HEAP_BLOCK_FREE;
+    first_block_ptr->size = heaps[kind].size - sizeof(cv64_heapblock_hdr_t);
+}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_free.s")
+void heap_free(cv64_heap_kind_t kind) { heaps[kind].flags = HEAP_INACTIVE; }
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_writebackDCache.s")
+void heap_writebackDCache(void) {
+    cv64_heap_inf_t* first;
+    cv64_heap_inf_t* current_heap;
+
+    first = &heaps[0], current_heap = &heaps[HEAP_NUM - 1];
+    do {
+        if (current_heap->flags & HEAP_WRITE_BACK_CACHE_TO_RAM) {
+            osWritebackDCache(current_heap->heap_start, current_heap->size);
+        }
+        current_heap--;
+    } while ((u32) first <= (u32) current_heap);
+}
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/initHeaps.s")
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80000D68_1968.s")
+void func_80000D68_1968(s32 arg0, u32 arg1) {}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80000D74_1974.s")
+s32 func_80000D74_1974(s32 arg0) { return 0; }
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_alloc.s")
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/heap_allocWithAlignment.s")
 
+// https://decomp.me/scratch/tHw91
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/heapBlock_updateBlockMaxSize.s")
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001008_1C08.s")
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/heapBlock_free.s")
 
+// https://decomp.me/scratch/eT3oh
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001080_1C80.s")
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_800010A0_1CA0.s")
@@ -53,32 +73,186 @@
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/memory/func_80001338_1F38.s")
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_curLevel_goToNextFunc.s")
+void object_curLevel_goToNextFunc(u16 current_functionInfo[],
+                                  s16* functionInfo_ID) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_prevLevel_goToNextFunc.s")
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    entry_to_clean = functionInfo_entry + 1;
+    (*functionInfo_entry)++; // Set current entry to next function
+    while (entry_to_clean < (u16*) functionInfo_ID) {
+        *entry_to_clean = 0;
+        entry_to_clean++;
+    }
+}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_nextLevel_goToNextFunc.s")
+void object_prevLevel_goToNextFunc(u16 current_functionInfo[],
+                                   s16* functionInfo_ID) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
 
-// clang-format off
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_curLevel_goToNextFuncAndClearTimer.s")
-// clang-format on
+    if (*functionInfo_ID > 0) {
+        functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+        functionInfo_entry--;
+        (*functionInfo_entry)++;
+        for (entry_to_clean = functionInfo_entry + 1;
+             entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+            *entry_to_clean = 0;
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_curLevel_goToPrevFunc.s")
+void object_nextLevel_goToNextFunc(u16 current_functionInfo[],
+                                   s16* functionInfo_ID) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_prevLevel_goToPrevFunc.s")
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    functionInfo_entry++;
+    if (functionInfo_entry < (u16*) functionInfo_ID) {
+        entry_to_clean = functionInfo_entry + 1;
+        (*functionInfo_entry)++;
+        while (entry_to_clean < (u16*) functionInfo_ID) {
+            *entry_to_clean = 0;
+            entry_to_clean++;
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_nextLevel_goToPrevFunc.s")
+void object_curLevel_goToNextFuncAndClearTimer(
+    cv64_obj_func_inf_t current_functionInfo[], s16* functionInfo_ID) {
+    cv64_obj_func_inf_t* functionInfo_entry;
+    cv64_obj_func_inf_t* entry_to_clean;
 
-// clang-format off
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_curLevel_goToPrevFuncAndClearTimer.s")
-// clang-format on
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    functionInfo_entry->whole++;   // Set current function to the next function
+    functionInfo_entry->timer = 0; // Set timer to 0
+    for (entry_to_clean = functionInfo_entry + 1;
+         (cv64_obj_func_inf_t*) entry_to_clean <
+         (cv64_obj_func_inf_t*) functionInfo_ID;
+         entry_to_clean++) {
+        entry_to_clean->whole = 0;
+    }
+}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_curLevel_goToFunc.s")
+void object_curLevel_goToPrevFunc(cv64_obj_func_inf_t current_functionInfo[],
+                                  s16* functionInfo_ID) {
+    cv64_obj_func_inf_t* functionInfo_entry;
+    u16* entry_to_clean;
 
-// clang-format off
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_curLevel_goToFuncInLevel.s")
-// clang-format on
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    functionInfo_entry->whole--;
+    for (entry_to_clean = (u16*) functionInfo_entry + 1;
+         entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+        *entry_to_clean = 0;
+    }
+}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_prevLevel_goToFunc.s")
+void object_prevLevel_goToPrevFunc(u16 current_functionInfo[],
+                                   s16* functionInfo_ID) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/memory/object_nextLevel_goToFunc.s")
+    if (*functionInfo_ID > 0) {
+        functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+        functionInfo_entry--;
+        (*functionInfo_entry)--;
+        for (entry_to_clean = functionInfo_entry + 1;
+             entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+            *entry_to_clean = 0;
+        }
+    }
+}
+
+void object_nextLevel_goToPrevFunc(u16 current_functionInfo[],
+                                   s16* functionInfo_ID) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
+
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    functionInfo_entry++;
+    if (functionInfo_entry < (u16*) functionInfo_ID) {
+        entry_to_clean = functionInfo_entry + 1;
+        (*functionInfo_entry)--;
+        while (entry_to_clean < (u16*) functionInfo_ID) {
+            *entry_to_clean = 0;
+            entry_to_clean++;
+        }
+    }
+}
+
+void object_curLevel_goToPrevFuncAndClearTimer(u16 current_functionInfo[],
+                                               s16* functionInfo_ID) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
+
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    (*functionInfo_entry)--;
+    (u8)(*functionInfo_entry) = 0;
+    for (entry_to_clean = functionInfo_entry + 1;
+         entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+        *entry_to_clean = 0;
+    }
+}
+
+void object_curLevel_goToFunc(u16 current_functionInfo[], s16* functionInfo_ID,
+                              s32 function) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
+
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    *functionInfo_entry = function;
+    for (entry_to_clean = functionInfo_entry + 1;
+         entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+        *entry_to_clean = 0;
+    }
+}
+
+void object_curLevel_goToFuncInLevel(u16 current_functionInfo[],
+                                     s16* functionInfo_ID, s16 level,
+                                     s32 function) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
+
+    functionInfo_entry = &current_functionInfo[level];
+    *functionInfo_entry = function;
+    for (entry_to_clean = functionInfo_entry + 1;
+         entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+        *entry_to_clean = 0;
+    }
+}
+
+void object_prevLevel_goToFunc(u16 current_functionInfo[], s16* functionInfo_ID,
+                               s32 function) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
+
+    if (*functionInfo_ID > 0) {
+        functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+        functionInfo_entry--;
+        *functionInfo_entry = function;
+        for (entry_to_clean = functionInfo_entry + 1;
+             entry_to_clean < (u16*) functionInfo_ID; entry_to_clean++) {
+            *entry_to_clean = 0;
+        }
+    }
+}
+
+void object_nextLevel_goToFunc(u16 current_functionInfo[], s16* functionInfo_ID,
+                               s32 function) {
+    u16* functionInfo_entry;
+    u16* entry_to_clean;
+
+    functionInfo_entry = &current_functionInfo[*functionInfo_ID];
+    functionInfo_entry++;
+    if (functionInfo_entry < (u16*) functionInfo_ID) {
+        entry_to_clean = functionInfo_entry + 1;
+        (*functionInfo_entry)--; // Unneeded
+        *functionInfo_entry = function;
+        while (entry_to_clean < (u16*) functionInfo_ID) {
+            *entry_to_clean = 0;
+            entry_to_clean++;
+        }
+    }
+}
