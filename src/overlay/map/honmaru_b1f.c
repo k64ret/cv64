76933303022ff196e9ab965e9a94e90209cd92a9
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-01-07 17:42:22 -0500

    refactor(overlay): rename `HONMARU_B1F` to lowercase

---
+++ src/overlay/map/honmaru_b1f.c
@@ -0,0 +1,60 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018EB10_6C86A0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018EB80_6C8710.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018EC54_6C87E4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018ECC4_6C8854.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018EDD4_6C8964.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018EE80_6C8A10.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018EEF8_6C8A88.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F0C8_6C8C58.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F138_6C8CC8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F234_6C8DC4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F2E0_6C8E70.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F368_6C8EF8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F458_6C8FE8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F4B0_6C9040.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F520_6C90B0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F638_6C91C8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F6FC_6C928C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F768_6C92F8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F770_6C9300.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018F794_6C9324.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FA08_6C9598.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FC40_6C97D0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FC80_6C9810.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FCE8_6C9878.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FD58_6C98E8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FE34_6C99C4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_b1f/func_8018FEFC_6C9A8C.s")
+
+// clang-format on
