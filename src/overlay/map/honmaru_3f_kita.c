bba6fa2c4a2b0da04183980c259af24c0637e7d1
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-01-07 17:49:27 -0500

    refactor(overlay): rename `HONMARU_3F_KITA` to lowercase

---
+++ src/overlay/map/honmaru_3f_kita.c
@@ -0,0 +1,14 @@
+#include "cv64.h"
+#include <ultra64.h>
+
+// clang-format off
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_3f_kita/func_8018EB10_6CE4B0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_3f_kita/func_8018EB20_6CE4C0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_3f_kita/func_8018EB9C_6CE53C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/map/honmaru_3f_kita/func_8018EC0C_6CE5AC.s")
+
+// clang-format on
