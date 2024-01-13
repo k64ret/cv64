7bbc36e0d1b096feeae3051b7dc48b68fa8241ca
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-01-13 16:37:48 -0500

    fix(overlay): switch includes in rose.h

--- include/game/objects/map/rose/rose.h
+++ include/game/objects/map/rose/rose.h
@@ -1,8 +1,11 @@
 #ifndef ROSE_H
 #define ROSE_H
 
-#include "objects/map/rose/door.h"
+// Turn off formatting to preserve include order
+// clang-format off
 #include "objects/map/rose/ventilator.h"
+#include "objects/map/rose/door.h"
+// clang-format on
 
 const char ROSE_unused_string_1[] = "VENTILATOR : Can't allocate F3D.\n";
 
