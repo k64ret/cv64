952bd6827a18a6f77b85947e6e73468e3ff8f8a8
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-03-08 19:59:54 -0500

    fix(effect_manager): get things building/matching again

---
+++ src/effect_manager.c
@@ -0,0 +1,14 @@
+#include "cv64.h"
+#include "objects/effect/effect_manager.h"
+
+// effectMgr* ptr_effectMgr;
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/effect_manager/effectMgr_entrypoint.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/effect_manager/effectMgr_init.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/effect_manager/effectMgr_loop.s")
+
+void func_8005AF8C(void) {}
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/effect_manager/func_8005AF94.s")
