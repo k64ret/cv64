ed784e91b2d010b165f2c4311394dd4872fc71b3
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-03-14 01:35:20 -0400

    docs(object): make comments into doc-comments

--- include/game/object.h
+++ include/game/object.h
@@ -9,17 +9,25 @@
 #define OBJECT_NUM_MAX   554
 
 // clang-format off
+
 typedef enum cv64_object_exec_flag {
     OBJ_EXEC_FLAG_DONT_DESTROY = 0x0010,
-    OBJ_EXEC_FLAG_PAUSE = 0x4000,
-    OBJ_EXEC_FLAG_TOP   = 0x8000
+    OBJ_EXEC_FLAG_PAUSE        = 0x4000,
+    OBJ_EXEC_FLAG_TOP          = 0x8000
 } cv64_object_exec_flag_t;
+
 // clang-format on
 
 typedef union cv64_object_func_inf {
     struct {
-        u8 timer;    // Could also be number of accesses to function
-        u8 function; // Function ID
+        /**
+         * Could also be number of accesses to function
+         */
+        u8 timer;
+        /**
+         * Function ID
+         */
+        u8 function;
     };
     u16 whole;
 } cv64_object_func_inf_t; // Size = 0x2
@@ -27,11 +35,17 @@ typedef union cv64_object_func_inf {
 typedef struct cv64_object_hdr_t {
     s16 ID;
     u16 flags;
-    s16 timer; // Misc. timer whose purpose depends on the object
+    /**
+     * Misc. timer whose purpose depends on the object
+     */
+    s16 timer;
     s16 field_0x06;
     cv64_object_func_inf_t current_function[3];
     s16 functionInfo_ID;
-    void (*destroy)(void*); // Officially called "OBJ_destruct"
+    /**
+     * Officially called "OBJ_destruct"
+     */
+    void (*destroy)(void*);
     struct cv64_object_hdr_t* parent;
     struct cv64_object_hdr_t* next;
     struct cv64_object_hdr_t* child;
@@ -40,10 +54,11 @@ typedef struct cv64_object_hdr_t {
 // Generic object struct
 #define OBJ_NUM_FIGURES    4
 #define OBJ_NUM_ALLOC_DATA 16
+
 typedef struct cv64_object_t {
     cv64_object_hdr_t header;
-    /** The following two variables are
-     * bitfields, where each bit represents
+    /**
+     * The following two variables are bitfields, where each bit represents
      * a non-NULL pointer (1) in `alloc_data`
      */
     u16 field_0x20; // Entries allocated with `heap_alloc`
@@ -59,8 +74,10 @@ typedef struct cv64_object_t {
 #define OBJECT_FILE_INFO_FLAG_LAST 0x40
 
 typedef struct cv64_object_file_info {
-    // Can be either a pointer to another struct that holds the information, or
-    // simply the NI file ID The first byte is used as a "flag"
+    /**
+     * Can be either a pointer to another struct that holds the information,
+     * or simply the NI file ID The first byte is used as a "flag"
+     */
     u32 addr_or_file_ID;
     u32 file_padding;
 } cv64_object_file_info_t;
@@ -173,7 +190,10 @@ void* func_8013B454_BE644(
 extern cv64_object_t objects_array[OBJECT_ARRAY_MAX];
 extern u16 objects_number_of_instances_per_object[OBJECT_NUM_MAX];
 extern cv64_object_file_info_t* objects_file_info[OBJECT_NUM_MAX];
-extern void (*Objects_functions[OBJECT_NUM_MAX])(cv64_object_hdr_t* self);
+
+typedef void (*cv64_object_func_t)(cv64_object_hdr_t* self);
+
+extern cv64_object_func_t Objects_functions[OBJECT_NUM_MAX];
 extern cv64_object_t* object_list_free_slot;
 
 // Mostly used inside entrypoint functions
