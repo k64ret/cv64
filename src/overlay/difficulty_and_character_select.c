a593d32b4954b9f23553c35d2b16a9253a1a60f1
moisesPC <moisesapc64@gmail.com>
2024-04-18 22:43:39 +0100

    Initial setup

---
+++ src/overlay/difficulty_and_character_select.c
@@ -0,0 +1,41 @@
+#include "cv64.h"
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_entrypoint.s"     \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/difficultySelect_loop.s"          \
+)
+
+#pragma GLOBAL_ASM(                                                                                   \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_areStructsCreated.s" \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_init.s"           \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_showScroll.s"     \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_createLens.s"     \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_selectOption.s"   \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_optionSelected.s" \
+)
+
+#pragma GLOBAL_ASM(                                                                                \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_destroy.s"        \
+)
+
+#pragma GLOBAL_ASM(                                                                                            \
+    "../asm/nonmatchings/overlay/difficulty_and_character_select/characterSelect_determineCharacterToSelect.s" \
+)
