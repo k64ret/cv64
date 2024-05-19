0362315055ea54e6b663ddb5370062342b4f5ef7
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-05-19 10:56:12 -0400

    feat(debug): match `processMeter_renderDivisions`

--- src/debug.c
+++ src/debug.c
@@ -1,6 +1,16 @@
 #include "cv64.h"
 #include "debug.h"
 
+extern s32 processMeter_number_of_divisions;
+extern f32 processBar_sizeDivisor;
+
+extern OSTime processMeter_greenBar_beginTime;
+extern OSTime processMeter_greenBar_endTime;
+extern OSTime processMeter_blueBar_beginTime;
+extern OSTime processMeter_blueBar_endTime;
+extern f32 processMeter_greenBarSize;
+extern f32 processMeter_blueBarSize;
+
 void func_80019BC0_1A7C0(void) {}
 
 void func_80019BC8_1A7C8(s32 arg0) {}
@@ -15,12 +25,47 @@ void func_80019BF8_1A7F8(void) {}
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_80019C00.s")
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_setSizeDivisor.s")
+void processMeter_setSizeDivisor(f32 size_divisor) {
+    processBar_sizeDivisor = size_divisor;
+}
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_update.s")
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_render.s")
+#ifdef NON_MATCHING
+    #pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_render.s")
+#else
+void processMeter_render(Gfx** dlist) {
+    f32 number_of_divisions = SCREEN_HEIGHT / processMeter_number_of_divisions;
+    f32 green_bar_size;
+    f32 blue_bar_size;
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_renderDivisions.s")
+    gDPPipeSync(dlist[0]++);
+    gDPSetCycleType(dlist[0]++, G_CYC_FILL);
+    gSPClearGeometryMode(dlist[0]++, G_ZBUFFER);
+    gDPSetRenderMode(dlist[0]++, G_RM_OPA_SURF, G_RM_OPA_SURF2);
+    green_bar_size = processMeter_greenBarSize / (16.67 * processMeter_number_of_divisions);
+    processMeter_renderBar(dlist, RGBA(3, 193, 3, 193), green_bar_size, 0);
+    blue_bar_size = processMeter_blueBarSize / (16.67 * processMeter_number_of_divisions);
+    processMeter_renderBar(dlist, RGBA(0, 31, 0, 31), blue_bar_size, 4);
+    processMeter_renderDivisions(dlist, number_of_divisions);
+    gDPPipeSync(dlist[0]++);
+}
+#endif
+
+void processMeter_renderDivisions(Gfx** dlist, f32 division_width) {
+    s32 i;
+
+    gDPPipeSync(dlist[0]++);
+    gDPSetFillColor(dlist[0]++, RGBA(0, 1, 0, 1));
+    for (i = 0; processMeter_number_of_divisions >= i; i++) {
+        gDPFillRectangle(
+            dlist[0]++,
+            (s32) division_width * i + 40,
+            SCREEN_HEIGHT - 30,
+            (s32) division_width * i + 40,
+            SCREEN_HEIGHT - 20
+        );
+    }
+}
 
 #pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_renderBar.s")
