e098d9a5a5672cb203fcde8d9bd880bfff0aa9a6
moisesPC <moisesapc64@gmail.com>
2024-05-19 21:37:06 +0100

    Match and finish 1AE50.c

--- include/cv64.h
+++ include/cv64.h
@@ -45,6 +45,7 @@ extern void func_8000C6D0();
 extern void func_80012400();
 extern void func_80005658();
 extern u32 getMapEventFlagID(s16 stage_ID);
+s32 func_8001A250_1AE50(s32* arg0, u16* arg1, s16 arg2);
 
 #define NPTR             0
 #define ARRAY_COUNT(arr) (s32)(sizeof(arr) / sizeof(arr[0])) // Get number of elements in the array
---
+++ src/1AE50.c
@@ -0,0 +1,52 @@
+#include "cv64.h"
+#include "system_work.h"
+
+s32 func_8001A250_1AE50(s32* arg0, u16* arg1, s16 arg2) {
+    s32 var_v0;
+    s32 var_v1 = 0;
+
+    if (arg2 < 0) {
+        arg2 *= -1;
+        var_v0 = 0;
+    } else {
+        var_v0 = 1;
+    }
+    if ((sys.controllers[0].buttons_pressed & CONT_UP) ||
+        (sys.controllers[0].buttons_pressed & CONT_DOWN)) {
+        *arg1 = 0;
+    }
+    if (sys.controllers[0].buttons_held & CONT_UP) {
+        if (*arg1 == 0) {
+            *arg1 = 3;
+            *arg0 = *arg0 - 1;
+            if (*arg0 < 0) {
+                var_v1 = -1;
+                if (var_v0 != 0) {
+                    *arg0 = arg2 - 1;
+                } else {
+                    *arg0 = 0;
+                }
+            }
+        } else {
+            *arg1 = *arg1 - 1;
+        }
+    }
+    if (sys.controllers[0].buttons_held & CONT_DOWN) {
+        if (*arg1 == 0) {
+            *arg1 = 3;
+            *arg0 = *arg0 + 1;
+            if (*arg0 >= arg2) {
+                if (var_v0 != 0) {
+                    *arg0  = 0;
+                    var_v1 = 1;
+                } else {
+                    *arg0  = arg2 - 1;
+                    var_v1 = 1;
+                }
+            }
+        } else {
+            *arg1 = *arg1 - 1;
+        }
+    }
+    return var_v1;
+}
