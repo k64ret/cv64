1dbcc854add5f2904a03699abe2cbedc61faf580
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-05-19 10:20:45 -0400

    feat(debug): create `src/debug.c`

---
+++ src/debug.c
@@ -0,0 +1,23 @@
+#include "cv64.h"
+
+void func_80019BC0_1A7C0(void) {}
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/func_80019BC8_1A7C8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/func_80019BD0_1A7D0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/func_80019BE4_1A7E4.s")
+
+void func_80019BF8_1A7F8(void) {}
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_80019C00.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_setSizeDivisor.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_update.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_render.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_renderDivisions.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/debug/processMeter_renderBar.s")
