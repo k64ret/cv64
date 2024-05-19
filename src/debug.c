1bc82b35fd1900b116a900d21f1045520920f9bc
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-05-19 10:34:02 -0400

    feat(debug): add empty function definitions

--- src/debug.c
+++ src/debug.c
@@ -1,12 +1,15 @@
 #include "cv64.h"
+#include "debug.h"
 
 void func_80019BC0_1A7C0(void) {}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/debug/func_80019BC8_1A7C8.s")
+void func_80019BC8_1A7C8(s32 arg0) {}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/debug/func_80019BD0_1A7D0.s")
+// Used in `func_80066400`. Maybe a stubbed debug print function
+void func_80019BD0_1A7D0(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {}
 
-#pragma GLOBAL_ASM("../asm/nonmatchings/debug/func_80019BE4_1A7E4.s")
+// Maybe another stubbed debug print function, based on the number of arguments
+void func_80019BE4_1A7E4(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {}
 
 void func_80019BF8_1A7F8(void) {}
 
