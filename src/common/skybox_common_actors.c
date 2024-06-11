e4935bfd6226966a3af533d5a929d3180153048f
moisesPC <moisesapc64@gmail.com>
2024-06-11 09:55:08 +0100

    Start skybox_common_actors

---
+++ src/common/skybox_common_actors.c
@@ -0,0 +1,27 @@
+#include "cv64.h"
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/obj0172_entrypoint.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/obj0172_init.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/obj0172_loop.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_entrypoint.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_init.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_main.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_main_idleDay.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_main_dayToNight.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_main_idleNight.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/commonMoon_main_nightToDay.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/obj8015C368_entrypoint.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/obj8015C368_init.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/skybox_common_actors/obj8015C368_loop.s")
