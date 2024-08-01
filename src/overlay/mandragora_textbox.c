bfe2868ae6dcbb32cbd011391905617338aa7a42
moisesPC <moisesapc64@gmail.com>
2024-08-01 18:59:55 +0100

    Begin source code file

---
+++ src/overlay/mandragora_textbox.c
@@ -0,0 +1,21 @@
+/**
+ * @file mandragora_textbox.c
+ *
+ * Handles the textbox that grants the Mandragora item to the player.
+ */
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E000000_E63080.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E000070_E630F0.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E000134_E631B4.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E0001CC_E6324C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E0002AC_E6332C.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E000338_E633B8.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E0003E0_E63460.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/overlay/mandragora_textbox/func_0E000404_E63484.s")
