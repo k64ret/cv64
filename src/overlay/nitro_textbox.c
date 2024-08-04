ea15857953fad3be8db7f1205ea03fc547d68901
moisesPC <moisesapc64@gmail.com>
2024-08-04 17:57:03 +0100

    Prepare YAML and symbol_addr for nitro textbox

---
+++ src/overlay/nitro_textbox.c
@@ -0,0 +1,17 @@
+#include "cv64.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000000_E63520.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000070_E63590.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000134_E63654.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000258_E63778.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E0002B4_E637D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000358_E63878.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000400_E63920.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay/nitro_textbox/func_0E000424_E63944.s")
