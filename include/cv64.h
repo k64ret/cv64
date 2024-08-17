4327bc04eaaf7a19025b5f17e54029f61b118fb4
moisesPC <moisesapc64@gmail.com>
2024-08-17 19:14:45 +0100

    Document text spots 70 and 71

--- include/cv64.h
+++ include/cv64.h
@@ -107,6 +107,9 @@ extern void* NisitenmaIchigoFiles_segmentToVirtual(u32 segment_address, s32 file
 #define SCREEN_WIDTH  320
 #define SCREEN_HEIGHT 240
 
+// Text IDs for Castle Wall (Main)
+#define CASTLE_WALL_MAIN_OPEN_GRATING           8
+#define CASTLE_WALL_MAIN_GRATING_ALREADY_OPENED 9
 // Text IDs for Castle Center (Main)
 #define CASTLE_CENTER_MAIN_WALL_INFO           2
 #define CASTLE_CENTER_MAIN_TAKE_MANDRAGORA     10
