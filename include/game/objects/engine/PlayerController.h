38d9ad4a727a2cd80e8e8004734d2bab02801f83
moisesPC <moisesapc64@gmail.com>
2024-08-22 13:48:25 +0100

    Add PlayerController struct definition

---
+++ include/game/objects/engine/PlayerController.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER_CONTROLLER_H
+#define PLAYER_CONTROLLER_H
+
+#include "object.h"
+
+// ID: 0x0007
+typedef struct {
+    cv64_object_hdr_t header;
+    u8 field_0x20[OBJECT_SIZE - 0x20];
+} PlayerController;
+
+void PlayerController_entrypoint(PlayerController* self);
+void PlayerController_createPlayerObject(PlayerController* self);
+
+typedef void (*PlayerController_func_t)(PlayerController*);
+
+extern PlayerController* ptr_PlayerController;
+
+#endif
