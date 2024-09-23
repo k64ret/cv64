bcf7d67997a80cdbf92f89f615ab7c02207543bf
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-09-23 00:07:01 -0400

    refactor: create separate header for bit macros

--- include/cv64.h
+++ include/cv64.h
@@ -1,6 +1,7 @@
 #ifndef CV64_H
 #define CV64_H
 
+#include "bit.h"
 #include "gfx/model_info.h"
 #include "math.h"
 #include <ultra64.h>
@@ -10,32 +11,6 @@ typedef u8 UNK8;
 typedef u16 UNK16;
 typedef s32 UNK32;
 typedef void* UNKPTR;
-
-#define BIT(num) (1 << (num))
-/**
- * Apply `mask` on top of `value`.
- */
-#define BITS_MASK(value, mask) ((value) & (mask))
-/**
- * Apply `mask` on top of `value` and assign result to `value`.
- */
-#define BITS_ASSIGN_MASK(value, mask) ((value) &= (mask))
-/**
- * Check if `bits` is set in `value`.
- */
-#define BITS_HAS(value, bits) BITS_MASK(value, bits)
-/**
- * Check if `bits` is set in `value`.
- */
-#define BITS_NOT_HAS(value, bits) !BITS_HAS(value, bits)
-/**
- * Set `bits` in `value`.
- */
-#define BITS_SET(value, bits) ((value) |= (bits))
-/**
- * Unset `bits` in `value`.
- */
-#define BITS_UNSET(value, bits) ((value) &= ~(bits))
 
 typedef u8 Addr[];
 
