91ce194de333458d3ff571d0611ed86bdb6bbe1a
moisesPC <moisesapc64@gmail.com>
2024-09-27 16:51:02 +0100

    Remove comments as this field may actually be unused

--- include/game/gfx/graphic_container.h
+++ include/game/gfx/graphic_container.h
@@ -6,10 +6,6 @@
 typedef struct GraphicContainerHeader {
     void* field_0x00;
     void* data_ptrs[NUM_GRAPHIC_BUFFERS];
-    /**
-     * Related to file decompression?
-     * See 0x80010298
-     */
     u8 field_0x0C[4];
 } GraphicContainerHeader;
 
