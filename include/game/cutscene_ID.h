e082ce460cc97529448eacf851e256c68929dea7
moisesPC <moisesapc64@gmail.com>
2024-10-04 20:09:32 +0100

    Add demo50 headers

--- include/game/cutscene_ID.h
+++ include/game/cutscene_ID.h
@@ -12,6 +12,7 @@ enum CutsceneID {
     CUTSCENE_ID_BLOW_UP_WALL_IN_BULL_ARENA = 0x14,
     CUTSCENE_ID_BLOW_UP_WALL_TO_LIBRARY    = 0x15,
     CUTSCENE_ID_FOREST_ACTIVATE_LEVER      = 0x1A,
+    CUTSCENE_ID_UNUSED_DEATH               = 0x32,
     CUTSCENE_ID_FOREST_INTRO               = 0x3C,
     CUTSCENE_ID_CREDITS                    = 0x63
 };
---
+++ include/game/objects/cutscene/demo50.h
@@ -0,0 +1,46 @@
+#ifndef DEMO50_H
+#define DEMO50_H
+
+#include "cutscene.h"
+#include "animation.h"
+
+typedef struct Demo50Data {
+    Model* death_model;
+    animationMgr death_anim_mgr;
+    Camera* cutscene_camera;
+    Model* player_model;
+    Camera* game_camera;
+    CutsceneCurrentCameraMovement current_camera_movement;
+    CutsceneCameraMovementState cam_mov_states[4];
+    DeathData* death_data;
+} Demo50Data;
+
+// ID: 0x2059
+typedef struct Demo50 {
+    ObjectHeader header;
+    u8 field_0x20[4];
+    Camera* cutscene_camera;
+    Death* death;
+    u8 field_0x2C[8];
+    Demo50Data* data;
+    u8 field_0x38[48];
+    u16 current_time;
+    s16 max_time;
+    u8 field_0x6C[4];
+    u8 skip_cutscene;
+    u8 set_player_anim_speed;
+    u8 state;
+    u8 field_0x73;
+} Demo50;
+
+extern void Demo50_Entrypoint(Demo50* self);
+extern void Demo50_Init(Demo50* self);
+extern void Demo50_CreateCutsceneCamera(Demo50* self);
+extern void Demo50_GetPlayerModelAndSetBorders(Demo50* self);
+extern void Demo50_Loop(Demo50* self);
+extern void Demo50_Restart(Demo50* self);
+extern void DEMO50_PlayDeathAnimations(Demo50* self, CutsceneCoordinatesConfig* coords);
+
+typedef void (*Demo50Func)(Demo50*);
+
+#endif
