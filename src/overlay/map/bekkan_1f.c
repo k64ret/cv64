ef6bf91ff7e7e755bdd6729c7ee94440a1110ea2
moisesPC <moisesapc64@gmail.com>
2024-11-15 16:35:49 +0000

    Docs

--- src/overlay/map/bekkan_1f.c
+++ src/overlay/map/bekkan_1f.c
@@ -1,3 +1,18 @@
+/**
+ * @file bekkan_1f.c
+ *
+ * Contains the code for objects exclusive to BEKKAN 1F
+ * (Villa Foyer)
+ *
+ * - Bekkan1FSquare: This object consists of a textureless square with no functionality
+ *                   other than displaying its model. Two of these are located behind the clouds
+ *                   in the room connecting the foyer with the rose garden, next to the entrance
+ *                   and exit doors.
+ *
+ * - DecorativeChandelier: This object is unused, and it just renders an unsolid chandelier model.
+ *                         Note that the chandelier from the foyer is part of another object entirely.
+ */
+
 #include "objects/map/bekkan_1f/decorative_chandelier.h"
 #include "objects/map/bekkan_1f/square.h"
 #include "gfx/figure.h"
@@ -35,6 +50,7 @@ void Bekkan1FSquare_Init(Bekkan1FSquare* self) {
     BITS_SET(model->flags, FIG_FLAG_APPLY_PRIMITIVE_COLOR);
     model->primitive_color.integer = sys.background_color.integer;
     (*Model_setMapActorModelNoCollision)(model, &BEKKAN_1F_SQUARE_DL);
+
     (*object_curLevel_goToNextFuncAndClearTimer)(
         self->header.current_function, &self->header.function_info_ID
     );
@@ -46,6 +62,7 @@ void Bekkan1FSquare_PauseTransformations(Bekkan1FSquare* self) {
     model = self->model;
     BITS_SET(model->flags, FIG_FLAG_PAUSE_TRANSFORMATIONS);
     (*Figure_UpdateMatrices)(model);
+
     (*object_curLevel_goToNextFuncAndClearTimer)(
         self->header.current_function, &self->header.function_info_ID
     );
@@ -72,6 +89,7 @@ void DecorativeChandelier_Init(DecorativeChandelier* self) {
     s32 temp;
     Model* model;
 
+    // Prepare decoration model
     model = (*Model_createAndSetChild)(
         FIG_TYPE_STANDALONE | FIG_TYPE_ALLOW_TRANSPARENCY_CHANGE, map_lights[0]
     );
@@ -85,6 +103,8 @@ void DecorativeChandelier_Init(DecorativeChandelier* self) {
     model->primitive_color.integer = sys.primitive_color.integer;
     model->fog_color.integer       = sys.background_color.integer;
     (*Model_setMapActorModelNoCollision)(model, &BEKKAN_1F_CHANDELIER_DECORATION_DL);
+
+    // Prepare pendant model
     model         = (*Model_createAndSetChild)(FIG_TYPE_STANDALONE, map_lights[0]);
     self->pendant = model;
     (*Actor_SetPosAndAngle)(self, model);
@@ -97,6 +117,7 @@ void DecorativeChandelier_Init(DecorativeChandelier* self) {
     model->primitive_color.integer = sys.primitive_color.integer;
     model->fog_color.integer       = sys.background_color.integer;
     (*Model_setMapActorModelNoCollision)(model, &BEKKAN_1F_CHANDELIER_PENDANT_DL);
+
     (*object_curLevel_goToNextFuncAndClearTimer)(
         self->header.current_function, &self->header.function_info_ID
     );
@@ -107,6 +128,7 @@ void DecorativeChandelier_PauseTransformations(DecorativeChandelier* self) {
 
     decoration_model = self->decoration;
     BITS_SET(decoration_model->flags, FIG_FLAG_PAUSE_TRANSFORMATIONS);
+
     (*object_curLevel_goToNextFuncAndClearTimer)(
         self->header.current_function, &self->header.function_info_ID
     );
