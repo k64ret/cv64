571236c7b38473eebf25ad0d92fbe1b6f1d0648b
moisesPC <moisesapc64@gmail.com>
2024-11-15 16:46:16 +0000

    Add obj 17c addresses

---
+++ src/common/object_017C.c
@@ -0,0 +1,7 @@
+#include "cv64.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/common/object_017C/obj017C_Entrypoint.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/common/object_017C/obj017C_Init.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/common/object_017C/obj017C_Loop.s")
