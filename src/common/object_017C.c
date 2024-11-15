3d8ee01b72655fb6ddac5a3338f7bb48ced45de1
moisesPC <moisesapc64@gmail.com>
2024-11-15 16:51:21 +0000

    Add headers obj17C

--- src/common/object_017C.c
+++ src/common/object_017C.c
@@ -1,7 +1,12 @@
-#include "cv64.h"
+#include "objects/map/object_017C.h"
+#include "system_work.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/common/object_017C/obj017C_Entrypoint.s")
+// clang-format off
 
-#pragma GLOBAL_ASM("asm/nonmatchings/common/object_017C/obj017C_Init.s")
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/object_017C/obj017C_Entrypoint.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/common/object_017C/obj017C_Loop.s")
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/object_017C/obj017C_Init.s")
+
+#pragma GLOBAL_ASM("../asm/nonmatchings/common/object_017C/obj017C_Loop.s")
+
+// clang-format on
